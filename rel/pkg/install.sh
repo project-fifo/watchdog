b4bde8262068e86f92aa947514fdcecdd8b5fe21
Heinz N. Gies <heinz@licenser.net>
2015-10-12 01:19:42 +0200

    Disable conf udpate

--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -44,10 +44,10 @@ case $2 in
             cp ${CONFFILE}.example ${CONFFILE}
             $SED -i bak -e "s/127.0.0.1/${IP}/g" ${CONFFILE}
         else
-            echo "Merging old file with new template, the original can be found in ${CONFFILE}.old."
-            /opt/local/fifo-watchdog/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
-                mv ${CONFFILE} ${CONFFILE}.old &&
-                mv ${CONFFILE}.new ${CONFFILE}
+            echo "Please make sure you update your config according to the update manual!"
+            #/opt/local/fifo-sniffle/share/update_config.sh ${CONFFILE}.example ${CONFFILE} > ${CONFFILE}.new &&
+            #    mv ${CONFFILE} ${CONFFILE}.old &&
+            #    mv ${CONFFILE}.new ${CONFFILE}
         fi
         ;;
 esac
